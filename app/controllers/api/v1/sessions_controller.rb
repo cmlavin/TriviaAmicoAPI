0eb5df4f4b57a72c9342444f5a37af29c41ff4c1
Catherine <cmlavin7@gmail.com>
2017-09-07 15:06:33 -0400

    Login function works properly.

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,12 +4,12 @@ class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
-      payload = { user_id: user_id }
+      payload = { user_id: user.id }
       token = issue_token(payload)
       render json: { jwt: token }
     else
       render json: { error: "Something went wrong." }
     end
   end
-  
+
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,9 +4,8 @@ class Api::V1::UsersController < ApplicationController
   def me
     render json: current_user
   end
-#username: params[:username], password: params[:password], email: params[:email]
+
   def create
-    #byebug
     @user = User.new(username: params[:username], password: params[:password], email: params[:email])
     if @user.save
       render json: @user
@@ -14,7 +13,6 @@ class Api::V1::UsersController < ApplicationController
       render json: { message: "User not created." }
     end
   end
-
 
   def user_params
     params.require(:user).permit(:username, :password, :email)
