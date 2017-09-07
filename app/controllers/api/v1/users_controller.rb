8259500d7ca1f879f9209d4bae31b2f5a8b4b291
Catherine <cmlavin7@gmail.com>
2017-09-07 14:29:42 -0400

    Successfully creating users from signup page in frontend.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,8 +4,9 @@ class Api::V1::UsersController < ApplicationController
   def me
     render json: current_user
   end
-  
+#username: params[:username], password: params[:password], email: params[:email]
   def create
+    #byebug
     @user = User.new(username: params[:username], password: params[:password], email: params[:email])
     if @user.save
       render json: @user
@@ -13,6 +14,7 @@ class Api::V1::UsersController < ApplicationController
       render json: { message: "User not created." }
     end
   end
+
 
   def user_params
     params.require(:user).permit(:username, :password, :email)
