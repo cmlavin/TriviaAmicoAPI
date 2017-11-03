00d42c95a05a2fe2a2f09ef551062add62e82e2d
Catherine <cmlavin7@gmail.com>
2017-11-03 17:23:54 -0400

    Fixed db seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
   User.create(username: username, password: password, email: email)
 end
 
-categories = ["Comics", "Sports", "Geography", "Art", "Music", "Computers", "Film", "Politics", "Video Games", "Board Games"]
+categories = ["29", "21", "22", "25", "12", "18", "11", "20", "15", "16"]
 categories.each do |category|
   Game.create(category: category, difficulty: "easy", num_questions: 4)
 end
